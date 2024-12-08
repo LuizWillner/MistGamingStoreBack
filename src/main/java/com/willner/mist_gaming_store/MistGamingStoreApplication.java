c1331c5831030cadf031f79992af2f9437bedaef
LuizWillner <luizwillner@gmail.com>
2024-12-08 20:06:46 -0300

    Initial commit

---
+++ src/main/java/com/willner/mist_gaming_store/MistGamingStoreApplication.java
@@ -0,0 +1,13 @@
+package com.willner.mist_gaming_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MistGamingStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MistGamingStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/willner/mist_gaming_store/MistGamingStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willner.mist_gaming_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MistGamingStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
