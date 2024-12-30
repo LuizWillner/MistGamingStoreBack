dc066a208eafd760e99aba6c98a43bced3846b6f
LuizWillner <luizwillner@gmail.com>
2024-12-30 19:53:12 -0300

    UserModel fazendo mapping com tabela user

--- src/main/java/com/willner/mist_gaming_store/model/UserModel.java
+++ src/main/java/com/willner/mist_gaming_store/model/UserModel.java
@@ -1,22 +1,39 @@
 package com.willner.mist_gaming_store.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 
 @Getter  // Anotação do Lombok para gerar automaticamente os métodos getters dos atributos de UserModel
 @Setter  // Anotação do Lombok para gerar automaticamente os métodos setters dos atributos de UserModel
 // @Data // Gera automaticamente tanto os métodos getters quanto os setters dos atributos de UserModel
+@Entity(name = "user")  // Indica que a classe UserModel é uma entidade do banco de dados
 public class UserModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     // @NotEmpty(message = "O 'Nome' deve ser informado.")
     private String email;
+
     private String username;
+
     private String password;
 
-    public UserModel(String email, String username, String password) {
-        this.email = email;
-        this.username = username;
-        this.password = password;
-    }
+    @CreationTimestamp
+    private LocalDateTime created_at;
+
+//    public UserModel(String email, String username, String password) {
+//        this.email = email;
+//        this.username = username;
+//        this.password = password;
+//    }
 
 }
