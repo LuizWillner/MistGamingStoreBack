4233e4ebb61687f5e88bbcb90289b3705e9e450e
LuizWillner <luizwillner@gmail.com>
2025-01-03 19:49:58 -0300

    Extends dos repositories especificado

--- src/main/java/com/willner/mist_gaming_store/repository/ICategoryRepository.java
+++ src/main/java/com/willner/mist_gaming_store/repository/ICategoryRepository.java
@@ -1,4 +1,9 @@
 package com.willner.mist_gaming_store.repository;
 
-public interface ICategoryRepository {
+import com.willner.mist_gaming_store.model.CategoryModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+// Primeiro param do Generator <> é o tipo da classe que o repository representa e o segundo é o tipo do ID
+public interface ICategoryRepository extends JpaRepository<CategoryModel, Long> {
 }
--- src/main/java/com/willner/mist_gaming_store/repository/IGameRepository.java
+++ src/main/java/com/willner/mist_gaming_store/repository/IGameRepository.java
@@ -1,4 +1,9 @@
 package com.willner.mist_gaming_store.repository;
 
-public interface IGameRepository {
+import com.willner.mist_gaming_store.model.GameModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+// Primeiro param do Generator <> é o tipo da classe que o repository representa e o segundo é o tipo do ID
+public interface IGameRepository extends JpaRepository<GameModel, Long> {
 }
