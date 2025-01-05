6eda768fc53e0f1618b05d1f74d47580da1adbc2
LuizWillner <luizwillner@gmail.com>
2025-01-05 18:19:33 -0300

    Classe principal implementa CommandLineRunner

--- src/main/java/com/willner/mist_gaming_store/MistGamingStoreApplication.java
+++ src/main/java/com/willner/mist_gaming_store/MistGamingStoreApplication.java
@@ -1,14 +1,19 @@
 package com.willner.mist_gaming_store;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class MistGamingStoreApplication {
+public class MistGamingStoreApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MistGamingStoreApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Aplicação rodando...");
+	}
 }
