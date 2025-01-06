ab5c92041709edb31ed979766d9ad18b385817d0
LuizWillner <luizwillner@gmail.com>
2025-01-06 14:42:37 -0300

    FEAT: Endpoint createCartItem

--- src/main/java/com/willner/mist_gaming_store/controller/CartItemController.java
+++ src/main/java/com/willner/mist_gaming_store/controller/CartItemController.java
@@ -1,4 +1,38 @@
 package com.willner.mist_gaming_store.controller;
 
+import com.willner.mist_gaming_store.model.CartItemModel;
+import com.willner.mist_gaming_store.service.CartItemService;
+import com.willner.mist_gaming_store.service.CartService;
+import com.willner.mist_gaming_store.service.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("user/cart/item")
 public class CartItemController {
+
+    @Autowired
+    private CartItemService cartItemService;
+
+    @Autowired
+    private CartService cartService;
+
+    @Autowired
+    private GameService gameService;
+
+
+
+    // Add produto ao carrinho
+    @PostMapping  // POST http://localhost:8080/user/cart/item?userId=1&cartId=1&gameId=1&quantity=1
+    public CartItemModel createCartItem(
+            @RequestParam(name = "userId") Long userId,
+            @RequestParam(name= "cartId") Long cartId,
+            @RequestParam(name= "gameId") Long gameId,
+            @RequestParam(name= "quantity", defaultValue = "1") int quantity
+    ) {
+        var game = this.gameService.findGameById(gameId);
+        var cart = this.cartService.getCartByIdFromUser(cartId, userId);
+        CartItemModel cartItem = new CartItemModel(quantity, cart, game);
+        return this.cartItemService.createCartItem(cartItem);
+    }
 }
--- src/main/java/com/willner/mist_gaming_store/service/CartItemService.java
+++ src/main/java/com/willner/mist_gaming_store/service/CartItemService.java
@@ -1,4 +1,19 @@
 package com.willner.mist_gaming_store.service;
 
+import com.willner.mist_gaming_store.model.CartItemModel;
+import com.willner.mist_gaming_store.model.CategoryModel;
+import com.willner.mist_gaming_store.repository.ICartItemRepository;
+import com.willner.mist_gaming_store.repository.ICategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CartItemService {
+
+    @Autowired
+    private ICartItemRepository cartItemRepository;
+
+    public CartItemModel createCartItem(CartItemModel cartItem) {
+        return this.cartItemRepository.save(cartItem);
+    }
 }
