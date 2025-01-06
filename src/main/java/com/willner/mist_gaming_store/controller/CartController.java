1430f1c69c5bf471c1fe761741b89dec0a5507da
LuizWillner <luizwillner@gmail.com>
2025-01-06 16:34:32 -0300

    FEAT: Endpoint finalizePurchase

--- src/main/java/com/willner/mist_gaming_store/controller/CartController.java
+++ src/main/java/com/willner/mist_gaming_store/controller/CartController.java
@@ -1,7 +1,10 @@
 package com.willner.mist_gaming_store.controller;
 
 import com.willner.mist_gaming_store.model.CartModel;
+import com.willner.mist_gaming_store.model.GameModel;
+import com.willner.mist_gaming_store.service.CartItemService;
 import com.willner.mist_gaming_store.service.CartService;
+import com.willner.mist_gaming_store.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +15,10 @@ public class CartController {
 
     @Autowired
     private CartService cartService;
+    @Autowired
+    private GameService gameService;
+    @Autowired
+    private CartItemService cartItemService;
 
     @GetMapping  // GET http://localhost:8080/user/cart?cartId=1&userId=1
     public CartModel getCartByIdFromUser(
@@ -29,7 +36,23 @@ public class CartController {
         return cartService.getCartByIdFromUser(cartId, userId).getTotalPrice();
     }
 
-
+    @PutMapping("/purchase")  // PUT http://localhost:8080/user/cart/purchase?cartId=1&userId=1
+    public CartModel finalizePurchase(
+            @RequestParam(name = "cartId") Long cartId,
+            @RequestParam(name = "userId") Long userId
+    ) {
+        CartModel cart = cartService.getCartByIdFromUser(cartId, userId);
+        cart.getCartItems().forEach(cartItem -> {
+            GameModel game = cartItem.getGame();
+            if (game.getStockQuantity() < cartItem.getQuantity()) {
+                throw new RuntimeException("Quantidade insuficiente em estoque para o jogo " + game.getName());
+            }
+            game.setStockQuantity(game.getStockQuantity() - cartItem.getQuantity());
+            gameService.updateGame(game);
+            cartItemService.deleteCartItem(cartItem.getCartItemId());
+        });
+        return cart;
+    }
 
     @DeleteMapping  // DELETE http://localhost:8080/user/cart?cartId=1&userId=1
     public CartModel deleteCartByIdFromUser(
--- src/main/java/com/willner/mist_gaming_store/repository/ICartItemRepository.java
+++ src/main/java/com/willner/mist_gaming_store/repository/ICartItemRepository.java
@@ -3,6 +3,7 @@ package com.willner.mist_gaming_store.repository;
 import com.willner.mist_gaming_store.model.CartItemModel;
 import com.willner.mist_gaming_store.model.CartModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
@@ -11,4 +12,8 @@ public interface ICartItemRepository extends JpaRepository<CartItemModel, Long> 
 
     @Query("select ci from cart_item ci where ci.cartItemId = :cartItemId AND ci.cart.cartId = :cartId")
     Optional<CartItemModel> findByIdAndCartId(Long cartItemId, Long cartId);
+
+    @Modifying  // Por alguma razão, tive que redefinir o deleteById pois o original não funcionava
+    @Query("delete from cart_item ci where ci.cartItemId = :cartItemId")
+    void deleteById(Long cartItemId);
 }
